b89a0f294345d399fbf75b925a8eeed4b380fb5f
Michael.Wood <michaelwood@enhancedsystems.com>
2025-01-03 12:27:45 -0500

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import "./css/style.css";
+import Script from 'next/script'
 
 import localFont from "next/font/local";
 import Header from "@/components/ui/header";
-import Footer from "@/components/ui/footer";
 
 const press2P = localFont({
   src: [
@@ -31,6 +31,20 @@ export default function RootLayout({
     <head>
       <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet"/>
       <link href="https://unpkg.com/nes.css/css/nes.css" rel="stylesheet" />
+      <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-8YM6P4B4XL"
+        />
+
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-8YM6P4B4XL');
+          `}
+        </Script>
     </head>
       <body
         className={`${press2P.variable} bg-transparent font-inter text-base text-gray-200 antialiased`}
